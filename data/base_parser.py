9e59803068441d72d70c1b9a89f85ce5defe1baa
Abbondanzo <peter@abbondanzo.com>
2018-09-20 14:06:23 -0400

    Error plz

--- data/base_parser.py
+++ data/base_parser.py
@@ -4,8 +4,12 @@ import carball
 from carball.json_parser.game import Game
 
 
-def get_decompiled_replay():
-    json = carball.decompile_replay(os.path.abspath('test/replays/516E50BF491D3ECE8D1E9FAD08C00F9B.replay'),
-                                    os.path.abspath('test/replays/516E50BF491D3ECE8D1E9FAD08C00F9B.json'), overwrite=False)
+def compile_to_json():
+    carball.decompile_replay(os.path.abspath('test/replays/516E50BF491D3ECE8D1E9FAD08C00F9B.replay'),
+                             os.path.abspath('test/replays/516E50BF491D3ECE8D1E9FAD08C00F9B.json'), overwrite=False)
 
-    return Game(loaded_json=json)
+
+def get_decompiled_replay():
+    game = Game(os.path.abspath(
+        'test/replays/516E50BF491D3ECE8D1E9FAD08C00F9B.json'))
+    return game
--- init.py
+++ init.py
@@ -2,9 +2,9 @@ import logging
 
 import data.base_parser as parser
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
     # Do things
     game = parser.get_decompiled_replay()
-    print(game)
+    print(game.replay_data)
